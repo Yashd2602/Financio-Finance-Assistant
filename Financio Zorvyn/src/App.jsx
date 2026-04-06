772cf8c591ac5e96006854b9686dc386176add1f
Yashd2602 <yashdubey2602@gmail.com>
2026-04-06 21:11:41 +0530

    refactor: make layout the default route

--- Financio Zorvyn/src/App.jsx
+++ Financio Zorvyn/src/App.jsx
@@ -1,23 +1,24 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./Components/Layout";
 import Overview from "./Components/Overview";
 import Transactions from "./Components/Transactions";
 import Insights from "./Components/Insights";
-import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-
-        <Route path="/dashboard" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Overview />} />
           <Route path="transactions" element={<Transactions />} />
           <Route path="insights" element={<Insights />} />
         </Route>
 
-        <Route path="*" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Navigate to="/" replace />} />
+        <Route path="/dashboard/transactions" element={<Navigate to="/transactions" replace />} />
+        <Route path="/dashboard/insights" element={<Navigate to="/insights" replace />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
